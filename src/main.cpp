b359a6973afaa5b36392541b3643a749fd8a880f
wild-arctic-fox <amusing.arctic.fox@gmail.com>
2023-03-11 21:19:00 +0200

    add button and connect button to led

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,42 @@
 #include <Arduino.h>
 
-int ledPin = 9;
+const int LED = 9;
+const int BUTTON = 2;
+
+boolean lastButton = LOW;
+boolean currentButton = LOW;
+
+bool ledOn = false;
+
 void setup()
 {
-  pinMode(ledPin, OUTPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+  digitalWrite(LED, HIGH);
+}
+
+boolean debounce(boolean last)
+{
+  boolean current = digitalRead(BUTTON);
+
+  if (last != current)
+  {
+    delay(5);
+    current = digitalRead(BUTTON);
+  }
+  return current;
 }
 
 void loop()
 {
-  digitalWrite(ledPin, HIGH);
-  delay(3000);
-  digitalWrite(ledPin, LOW);
-  delay(3000);
+  currentButton = debounce(lastButton);
+
+  if (lastButton == LOW && currentButton == HIGH)
+  {
+    ledOn = !ledOn;
+  }
+
+  lastButton = currentButton;
+
+  digitalWrite(LED, ledOn);
 }
