b6623fd90b90088c23966039a0a9933790b5d1b6
wild-arctic-fox <amusing.arctic.fox@gmail.com>
2023-03-11 21:07:51 +0200

    One LED 3sec blink

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+int ledPin = 9;
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(ledPin, HIGH);
+  delay(3000);
+  digitalWrite(ledPin, LOW);
+  delay(3000);
+}
