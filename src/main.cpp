6c592b80b39844c50047d7c57044689fd6e0a747
wild-arctic-fox <amusing.arctic.fox@gmail.com>
2023-03-18 00:14:11 +0200

    add float rgb

--- src/main.cpp
+++ src/main.cpp
@@ -18,79 +18,41 @@ void setup()
   pinMode(BUTTON, INPUT);
 }
 
-boolean debounce(boolean last)
-{
-  boolean current = digitalRead(BUTTON);
-  if (last != current)
-  {
-    delay(5);
-    current = digitalRead(BUTTON);
-    return current;
-  }
-}
-
-void setMode(int mode)
-{
-  if (mode == 1)
-  {
-    digitalWrite(RLED, HIGH);
-    digitalWrite(BLED, LOW);
-    digitalWrite(GLED, LOW);
-  }
-  else if (mode == 2)
-  {
-    digitalWrite(RLED, LOW);
-    digitalWrite(BLED, HIGH);
-    digitalWrite(GLED, LOW);
-  }
-  else if (mode == 3)
-  {
-    digitalWrite(RLED, LOW);
-    digitalWrite(BLED, LOW);
-    digitalWrite(GLED, HIGH);
-  }
-  else if (mode == 4)
-  {
-    analogWrite(RLED, 127);
-    analogWrite(BLED, 0);
-    analogWrite(GLED, 127);
-  }
-  else if (mode == 5)
-  {
-    analogWrite(RLED, 0);
-    analogWrite(BLED, 127);
-    analogWrite(GLED, 127);
-  }
-  else if (mode == 6)
-  {
-    analogWrite(RLED, 127);
-    analogWrite(BLED, 127);
-    analogWrite(GLED, 0);
-  }
-  else if (mode == 7)
-  {
-    analogWrite(RLED, 215);
-    analogWrite(BLED, 20);
-    analogWrite(GLED, 20);
-  }
-  else
-  {
-    analogWrite(RLED, LOW);
-    analogWrite(BLED, LOW);
-    analogWrite(GLED, LOW);
-  }
-}
-
 void loop()
 {
-  currentButton = debounce(lastButton);
-  if (lastButton == LOW && currentButton == HIGH)
-  {
-    ledMode++;
-  }
-  lastButton = currentButton;
 
-  if (ledMode == 8)
-    ledMode = 0;
-  setMode(ledMode);
+  for (int i = 0, j = 255; i < 255, j >= 0; i++, j--)
+  {
+    analogWrite(GLED, i);
+    analogWrite(BLED, j);
+    delay(10);
+  }
+
+  for (int i = 0, j = 255; i < 255, j >= 0; i++, j--)
+  {
+    analogWrite(BLED, i);
+    analogWrite(GLED, j);
+    delay(10);
+  }
+
+  for (int i = 0, j = 255; i < 255, j >= 0; i++, j--)
+  {
+    analogWrite(RLED, i);
+    analogWrite(BLED, j);
+    delay(10);
+  }
+
+  for (int i = 0, j = 255; i < 255, j >= 0; i++, j--)
+  {
+    analogWrite(GLED, i);
+    analogWrite(RLED, j);
+    delay(10);
+  }
+
+  for (int i = 0, j = 255; i < 255, j >= 0; i++, j--)
+  {
+    analogWrite(BLED, i);
+    analogWrite(GLED, j);
+    delay(10);
+  }
 }
