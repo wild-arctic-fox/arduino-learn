02395a500582985528f1de9e4d9a8d9997ab8208
wild-arctic-fox <amusing.arctic.fox@gmail.com>
2023-03-18 00:35:19 +0200

    add potentiometer, that change voltage between 0 and 5

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,17 @@
 #include <Arduino.h>
 
-const int BLED = 11;
-const int GLED = 10;
-const int RLED = 9;
-const int BUTTON = 2;
-
-boolean lastButton = LOW;
-boolean currentButton = LOW;
-int ledMode = 0;
+const int POD = 0;
+int val = 0;
 
 void setup()
 {
 
-  pinMode(RLED, OUTPUT);
-  pinMode(BLED, OUTPUT);
-  pinMode(GLED, OUTPUT);
-  pinMode(BUTTON, INPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-
-  for (int i = 0, j = 255; i < 255, j >= 0; i++, j--)
-  {
-    analogWrite(GLED, i);
-    analogWrite(BLED, j);
-    delay(10);
-  }
-
-  for (int i = 0, j = 255; i < 255, j >= 0; i++, j--)
-  {
-    analogWrite(BLED, i);
-    analogWrite(GLED, j);
-    delay(10);
-  }
-
-  for (int i = 0, j = 255; i < 255, j >= 0; i++, j--)
-  {
-    analogWrite(RLED, i);
-    analogWrite(BLED, j);
-    delay(10);
-  }
-
-  for (int i = 0, j = 255; i < 255, j >= 0; i++, j--)
-  {
-    analogWrite(GLED, i);
-    analogWrite(RLED, j);
-    delay(10);
-  }
-
-  for (int i = 0, j = 255; i < 255, j >= 0; i++, j--)
-  {
-    analogWrite(BLED, i);
-    analogWrite(GLED, j);
-    delay(10);
-  }
+  val = analogRead(POD);
+  Serial.println(val);
+  delay(1000);
 }
