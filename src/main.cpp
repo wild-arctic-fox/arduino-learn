221d821d2a61e1cf2eae9f0a1289af0e39c264d0
wild-arctic-fox <amusing.arctic.fox@gmail.com>
2023-04-08 19:14:03 +0300

    added temperature indicator

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,19 @@
 #include <Arduino.h>
 
-const int BLED = 11;
-const int GLED = 10;
-const int RLED = 9;
-const int BUTTON = 2;
-
-boolean lastButton = LOW;
-boolean currentButton = LOW;
-int ledMode = 0;
+const int TEMP = 0;
 
 void setup()
 {
-
-  pinMode(RLED, OUTPUT);
-  pinMode(BLED, OUTPUT);
-  pinMode(GLED, OUTPUT);
-  pinMode(BUTTON, INPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-
-  for (int i = 0, j = 255; i < 255, j >= 0; i++, j--)
-  {
-    analogWrite(GLED, i);
-    analogWrite(BLED, j);
-    delay(10);
-  }
-
-  for (int i = 0, j = 255; i < 255, j >= 0; i++, j--)
-  {
-    analogWrite(BLED, i);
-    analogWrite(GLED, j);
-    delay(10);
-  }
-
-  for (int i = 0, j = 255; i < 255, j >= 0; i++, j--)
-  {
-    analogWrite(RLED, i);
-    analogWrite(BLED, j);
-    delay(10);
-  }
-
-  for (int i = 0, j = 255; i < 255, j >= 0; i++, j--)
-  {
-    analogWrite(GLED, i);
-    analogWrite(RLED, j);
-    delay(10);
-  }
-
-  for (int i = 0, j = 255; i < 255, j >= 0; i++, j--)
-  {
-    analogWrite(BLED, i);
-    analogWrite(GLED, j);
-    delay(10);
-  }
+  int reading = analogRead(TEMP);
+  float voltage = reading * 5.0;
+  voltage /= 1024;
+  Serial.println("voltage: " + String(voltage));
+  float temperature = voltage * 100;
+  Serial.println("temp: " + String(temperature));
+  delay(2000);
 }
